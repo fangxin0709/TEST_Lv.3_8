0119ab862b90530f3c6eaa7d8d9dee09f69526b8
fangxin0709 <167990114+fangxin0709@users.noreply.github.com>
2024-09-14 08:06:45 +0800

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -1,27 +1,25 @@
 ﻿Public Class Form1
-    Dim d(100, 5), rno
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Call rdata()
-        Call sp1()
-        Call wdata()
-    End Sub
-
+    Dim d(100, 5), idx
     Sub rdata()
         FileOpen(8, "C:\Users\dora0\Desktop\8.txt", OpenMode.Input)
-        rno = 0
+        idx = 0
         Do While Not EOF(8)
-            rno = rno + 1
-            For j = 1 To 5
-                Input(8, d(rno, j))
+            idx += 1
+            For i = 1 To 5
+                Input(8, d(idx, i))
             Next
         Loop
         FileClose(8)
     End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call rdata()
+        Call sp1()
+        Call wdata()
+    End Sub
     Sub sp1()
         Dim m1, m2, m3
-
-        For i = 1 To rno
+        For i = 1 To idx
             Select Case d(i, 3)
                 Case "+"
                     m1 = d(i, 1) * d(i, 5) + d(i, 2) * d(i, 4)
@@ -36,13 +34,12 @@
                     m1 = d(i, 1) * d(i, 5)
                     m2 = d(i, 2) * d(i, 4)
             End Select
-
             For j = 2 To Math.Abs(m1)
                 Do While m1 Mod j = 0 And m2 Mod j = 0
-                    m1 = m1 / j : m2 = m2 / j
+                    m1 = m1 / j
+                    m2 = m2 / j
                 Loop
             Next
-
             m3 = m1 & "/" & m2
             If m1 = 0 Then m3 = 0
             If m2 = 1 Then m3 = m1
@@ -50,23 +47,22 @@
         Next
     End Sub
     Sub wdata()
-        Dim dt As New DataTable
-        dt.Columns.Add("VALUE1")
-        dt.Columns.Add("OP")
-        dt.Columns.Add("VALUE2")
-        dt.Columns.Add("ANSWER")
-
-        For i = 1 To rno
-            Dim dr As DataRow = dt.NewRow
-            dr(0) = d(i, 1) & "/" & d(i, 2)
-            dr(1) = d(i, 3)
-            dr(2) = d(i, 4) & "/" & d(i, 5)
-            dr(3) = d(i, 0)
-            dt.Rows.Add(dr)
+        Dim table As New DataTable
+        table.Columns.Add("VALUE1")
+        table.Columns.Add("OP")
+        table.Columns.Add("VALUE2")
+        table.Columns.Add("ANSWER")
+        For i = 1 To idx
+            Dim tr As DataRow = table.NewRow
+            tr(0) = d(i, 1) & "/" & d(i, 2)
+            tr(1) = d(i, 3)
+            tr(2) = d(i, 4) & "/" & d(i, 5)
+            tr(3) = d(i, 0)
+            table.Rows.Add(tr)
         Next
-
-        dgv.DataSource = dt
+        dgv.DataSource = table
         dgv.DefaultCellStyle.Alignment = 32
+
 
 
     End Sub
